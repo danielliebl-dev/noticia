7f71fdb3d400a33abeb73de7e2f0665f5ab3f839
danielliebl-dev <daniel.liebl@escola.pr.gov.br>
2026-06-26 09:54:04 -0300

    Add click event to display message

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const botao = document.getElementById("botao");
+const mensagem = document.getElementById("mensagem");
 
+botao.addEventListener("click", () => {
+    mensagem.style.display = "block";
+    mensagem.innerHTML = `
+        <strong>Atualização:</strong><br><br>
+        Esta reportagem faz parte de um projeto fictício de HTML, CSS e JavaScript.
+        O vírus "Paraochão" não existe e nenhum dos acontecimentos descritos ocorreu
+        na vida real.
+    `;
+});
